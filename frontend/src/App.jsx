c10154eb4333a906250399ccf4b67d289396492a
Jaypal-Singh <jaypalsinghchouhan2008@gmail.com>
2026-02-18 22:03:02 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Trade from "./Pages/trade.jsx";
 import LandingRoute from "./utils/LandingRoute/LandingRoute.jsx";
+import { SocketProvider } from "./context/SocketContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/trade/*" element={<Trade />} />
-        <Route path="/*" element={<LandingRoute />} />
-      </Routes>
-    </BrowserRouter>
+    <SocketProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/trade/*" element={<Trade />} />
+          <Route path="/*" element={<LandingRoute />} />
+        </Routes>
+      </BrowserRouter>
+    </SocketProvider>
   );
 }
 
